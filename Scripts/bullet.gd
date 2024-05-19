530ad39615c15dbc008905a7e78de4066b2bd1d5
mannygarcia720 <37597363+mannygarcia720@users.noreply.github.com>
2024-05-19 11:46:26 -0700

    Revert "Merge branch 'main' of https://github.com/LunafreyaNguyen/BellJam"
    This reverts commit 0e0fc8417b4b9c9ed11ba44e291c2b1e38f44f2a, reversing
    changes made to dadf2c5e2f910d9dc6e88166016fff2ad787928e.

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -2,10 +2,9 @@ extends Area2D
 @onready var sprite = $Sprite2D
 @onready var player = get_tree().get_first_node_in_group("Player")
 
-const speed = 3000.0
+const speed = 2500.0
 var area_direction = Vector2(0, 0)
 var debounce = false
-var progress = 1
 	
 var timer = 0
 
@@ -17,7 +16,10 @@ func _process(delta):
 	position += transform.x * speed * delta
 	if timer > 4:
 		queue_free()
-
+	if(position.x < 500 || position.x > 1420):
+		queue_free()
+	if(position.y < 0 || position.y > 1080):
+		queue_free()
 
 func _on_body_entered(body):	
 	# Stops an error that crashes the game.
@@ -29,7 +31,7 @@ func _on_body_entered(body):
 # Delete the bullet, lower enemy's HP
 func hit(body):
 	if body.is_in_group("Enemy"):
-		body.hit(progress)
-		player.styleProgress += progress
+		body.hit()
+		player.styleProgress += 1
 	if!(body.is_in_group("bullet")):
 		queue_free()
